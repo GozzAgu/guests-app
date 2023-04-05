432a3ee88c6b3dd6943e8e3165e660036feb4e89
GozzAgu <103033742+GozzAgu@users.noreply.github.com>
2023-04-05 05:41:29 +0100

    feat: created auth screens

---
+++ src/components/SignUp.vue
@@ -0,0 +1,210 @@
+<template>
+    <div class=
+        "bg-blue-100
+        p-5 
+        h-screen"
+    >
+        <div 
+            class=
+            "mt-4"
+        >
+            <img src="../assets/Guest-Logo.png" 
+                class=
+                "w-60 
+                m-auto" 
+            />
+        </div>
+
+        <div class=
+            "bg-white 
+            rounded-3xl 
+            shadow-sm
+            pt-5 
+            pb-5 
+            w-full 
+            md:w-80 
+            md:h-100 
+            lg:h-100 
+            lg:w-1/2 
+            m-auto 
+            flex 
+            items-center 
+            justify-center 
+            mt-20"
+        >
+            <div>
+                <div class=
+                    "flex 
+                    justify-center"
+                >
+                    <h1 class=
+                        "pt-4 
+                        text-3xl 
+                        text-gray-600 
+                        font-bold mb-8"
+                    >
+                        Sign Up
+                    </h1>
+                </div>
+
+                <div class=
+                    "flex 
+                    justify-center"
+                >
+                    <h1 class=
+                        "p-5
+                        w-80 
+                        text-center
+                        text-1xl 
+                        text-gray-400 
+                        font-semibold 
+                        mb-8"
+                    >
+                        Hey, Enter your credentials to create an account
+                    </h1>
+                </div>
+
+                <div class=
+                    "mt-4 flex 
+                    justify-center"
+                >
+                    <input class=
+                        "w-60 
+                        p-1 
+                        text-gray-400
+                        border-b-2 
+                        border-gray-200 
+                        focus:outline-none 
+                        focus:border-b-blue-200" 
+                        type="text" 
+                        placeholder="Username"
+                        v-model="user.displayName"
+                    />
+                </div>
+
+                <div class=
+                    "mt-4 flex 
+                    justify-center"
+                >
+                    <input class=
+                        "w-60 
+                        p-1 
+                        text-gray-400
+                        border-b-2 
+                        border-gray-200 
+                        focus:outline-none 
+                        focus:border-b-blue-200" 
+                        type="text" 
+                        placeholder="Email"
+                        v-model="user.email"
+                    />
+                </div>
+
+                <div class=
+                    "mt-4 flex 
+                    justify-center"
+                >
+                    <input class=
+                        "w-60 
+                        p-1 
+                        text-gray-400
+                        border-b-2 
+                        border-gray-200 
+                        focus:outline-none 
+                        focus:border-b-blue-200" 
+                        type="text" 
+                        placeholder="Password"
+                        v-model="user.password"
+                    />
+                </div>
+
+                <div class=
+                    "flex 
+                    justify-center 
+                    pb-4 
+                    mt-12"
+                >
+                    <router-link to="/" class=
+                        "w-60 
+                        text-center
+                        cursor-pointer
+                        bg-blue-400 
+                        text-white 
+                        pt-1 
+                        pb-1 
+                        pl-5 
+                        pr-5 
+                        rounded 
+                        hover:bg-blue-500 
+                        transition 
+                        delay-50 
+                        duration-500 
+                        transform"
+                        @click="signUp"
+                    >
+                        Sign Up
+                    </router-link>
+                </div>
+
+                <div class=
+                    "flex 
+                    justify-center 
+                    pb-4 
+                    mb-6"
+                >
+                    <router-link to='/' class=
+                        "w-60 
+                        text-center
+                        cursor-pointer
+                        border-2 
+                        pt-1 
+                        pb-1 
+                        pl-5 
+                        pr-5 
+                        rounded 
+                        text-blue-400 
+                        hover:border-blue-400 
+                        transition 
+                        delay-50 
+                        duration-500 
+                        transform"
+                    >
+                        Don't have an account ?
+                    </router-link>
+                </div>
+            </div>
+        </div>
+        <div class="flex justify-around text-gray-400 mt-4 w-80 m-auto">
+            <p>Copyright</p>
+            <p>Privacy policy</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { auth } from '../main';
+
+const user = ref({
+    displayName: '',
+    email: '',
+    password: ''
+})
+
+const signUp = () => {
+    createUserWithEmailAndPassword(auth, user.value.email, user.value.password)
+    .then((credential) => {
+        updateProfile(auth.currentUser, {
+            displayName: user.value.displayName
+        }) 
+        .then(() => {
+            console.log(auth.currentUser.displayName)
+            console.log(credential)
+        })
+    })
+    .catch((error) => {
+        console.log(error.message)
+    })
+}
+</script>
