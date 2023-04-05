731dbc89ecc9cb569f69e8fe3fde02a79c7b8641
GozzAgu <103033742+GozzAgu@users.noreply.github.com>
2023-04-05 05:41:48 +0100

    feat: created nav and footer components

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- <div class="flex justify-center fixed inset-x-0 -bottom-6"> -->
+        <div class="text-center fixed inset-x-0 bottom-10">
+            <i class="ri-user-add-fill text-blue-300 text-3xl p-6 shadow-lg rounded-full bg-blue-50"></i>
+        </div>
+        
+    <!-- </div> -->
+</template>
+
+<script setup>
+// import { ref, onMounted } from 'vue';
+
+
+</script>
---
+++ src/components/NavComponent.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="m-auto flex justify-around shadow-md p-3 text-gray-700 text-s">
+        <div>
+            <img src="../assets/Guest-Logo.png"
+                class=
+                "w-40 
+                mt-2" 
+            />
+        </div>
+        
+        <div v-if="isLoggedIn" class="mt-2">
+            <router-link to="/signin" 
+                class=
+                "w-60 
+                text-center
+                cursor-pointer
+                bg-blue-400 
+                text-white
+                pt-1 
+                pb-1 
+                pl-5 
+                pr-5 
+                rounded 
+                hover:bg-blue-500 
+                transition 
+                delay-50 
+                duration-500 
+                transform"
+                @click="logout"
+            >
+                SignOut
+            </router-link> 
+        </div>
+
+        <div v-else class="mt-2">
+            <router-link to="/signin" 
+                class=
+                "w-60 
+                text-center
+                cursor-pointer
+                bg-blue-400 
+                text-white
+                pt-1 
+                pb-1 
+                pl-5 
+                pr-5 
+                rounded 
+                hover:bg-blue-500 
+                transition 
+                delay-50 
+                duration-500 
+                transform"
+            >
+                SignIn
+            </router-link> 
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { auth } from '@/main';
+import { onAuthStateChanged, signOut } from '@firebase/auth';
+
+const isLoggedIn = ref(false);
+
+onMounted(() => {
+  onAuthStateChanged(auth, (user) => {
+    if(user) {
+      isLoggedIn.value = true;
+    } else {
+      isLoggedIn.value = false;
+    }
+    console.log(user);
+  })
+});
+
+const logout = () => {
+  signOut(auth);
+  console.log(auth)
+}
+</script>
