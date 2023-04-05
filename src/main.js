4339818cb1d34d16f42b4c96f3b6bcaea8c6b2ec
GozzAgu <103033742+GozzAgu@users.noreply.github.com>
2023-04-05 05:38:53 +0100

    feat: installed and created firebase config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import {initializeApp} from 'firebase/app'
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import 'remixicon/fonts/remixicon.css';
 
-createApp(App).use(router).mount('#app')
+const config = {
+    apiKey: 'AIzaSyAH-o0-TVc4p6TVuJAvhGgnP2vgDaYuSWA',
+    authDomain: "gateway-5fc9c.firebaseapp.com",
+    projectId: "gateway-5fc9c",
+    storageBucket: "gateway-5fc9c.appspot.com",
+    messagingSenderId: "855699634672",
+    appId: "1:855699634672:web:f11d520506ee6875590535",
+    measurementId: "G-TZSJ8Q2E0C"
+  };
+
+createApp(App)
+.use(router)
+.mount('#app')
+
+const firebase = initializeApp(config);
+
+export const db = getFirestore(firebase);
+export const auth = getAuth();
