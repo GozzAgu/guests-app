170028707e67ee393657322b84180473c14ebb14
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2023-04-05 09:30:25 +0100

    feat: added tailwind font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Oswald'],
+      'body': ['"Open Sans"']
+    }
   },
   plugins: [],
 }
