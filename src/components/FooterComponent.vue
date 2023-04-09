6c48c8b35dc5c0357568a3edd1f44fa44f58d853
GozzAgu <103033742+GozzAgu@users.noreply.github.com>
2023-04-09 10:00:54 +0100

    feat: added a draggable to the add button

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,15 +1,16 @@
 <template>
     <!-- <div class="flex justify-center fixed inset-x-0 -bottom-6"> -->
-        <div class="text-center fixed inset-x-0 bottom-10">
-            <i @click="openModal" class="ri-user-add-fill text-blue-300 text-xl p-6 shadow-lg rounded-full bg-blue-50"></i>
+        <div class="text-center fixed inset-x-0 bottom-10" ref="draggableContainer">
+            <i draggable="true" ref="grabBar" @mousedown="mousedown" id="btn" @click="openModal" class="ri-user-add-fill text-blue-300 text-xl p-6 shadow-lg rounded-full bg-blue-50"></i>
         </div>
         
     <!-- </div> -->
 </template>
 
 <script setup>
-// import { ref, onMounted } from 'vue';
-import { defineEmits } from 'vue';
+import { defineEmits, ref } from 'vue';
+
+let draggableContainer = ref(null)
 
 const emit = defineEmits(['modal']);
 
@@ -17,5 +18,32 @@ const openModal = () => {
     emit('modal');
 }
 
+// const mousedown = (event) => {
+//     console.log(event.clientX, event.clientY);
+//     document.querySelector('#btn').style.padding = '500px';
+// }
 
+const mousedown = (e) => {
+    window.addEventListener('mousemove', mousemove);
+      window.addEventListener('mouseup', mouseup);
+
+      let prevX = e.clientX;
+      let prevY = e.clientY;
+
+      function mousemove (e) {
+        const newX = prevX - e.clientX;
+        const newY = prevY - e.clientY;
+
+        const rect = draggableContainer.value.getBoundingClientRect();
+        draggableContainer.value.style.left = rect.left - newX + 'px';
+        draggableContainer.value.style.top = rect.top - newY + 'px';
+
+        prevX = e.clientX;
+        prevY = e.clientY;
+      }
+      function mouseup () {
+        window.removeEventListener('mousemove', mousemove);
+        window.removeEventListener('mouseup', mouseup);
+      };
+}
 </script>
