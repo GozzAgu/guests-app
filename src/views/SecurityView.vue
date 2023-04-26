36d5e310e9250f40419606f779a86ad5a58c802d
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2023-04-26 11:01:52 +0100

    fix: security page styled

--- src/views/SecurityView.vue
+++ src/views/SecurityView.vue
@@ -5,133 +5,91 @@
         <p class="font-bold text-gray-600 mt-2 animate-bounce">App in progress</p>
         <div class="loader m-auto mt-3"></div>
       </div>
+
+      <div class="guests max-w-5xl mx-auto mt-8">
+      <div class="p-3 flex justify-between md:justify-around lg:justify-around bg-blue-50 m-5 rounded-lg">
+        <p class="welcome-text font-bold text-gray-600"> <i @click="showTrackModal=true" class="ri-shield-user-line text-blue-400"></i> Welcome, {{ displayName }}</p>
+        <div class="">
+          <button @click="toggleDark">
+            <i v-if="isDark" class="ri-sun-fill text-2xl text-orange-200"></i>
+            <i v-else class="ri-moon-clear-fill text-2xl text-gray-400"></i>
+          </button>
+        </div>  
+      </div>
+    </div>
   
-      <div class="guests bg-blue-50 m-5 lg:ml-60 lg:mr-60 rounded-2xl">
-        <div class="p-5 flex justify-between md:justify-around lg:justify-around">
-          <h1 class="text-center font-bold text-gray-600 mt-1 guest-text"><i class="ri-file-user-fill"></i> GUESTS </h1>
-          
-          <div class="flex bg-white border-2 rounded-lg gap-x-1 w-40">
-            <i class="ri-search-2-line pl-2 mt-1"></i>
-            <input v-model="search" class="p-1 w-full focus:outline-none" type="text" placeholder="search for guest" />
+      <div class="guests max-w-5xl mx-auto mt-8">
+        <div class="p-3 flex justify-between md:justify-around lg:justify-around bg-blue-100 m-5 rounded-lg gap-x-2">
+          <h1 class="text-center font-semibold text-gray-600 guest-text mt-1"><i class="guest-text ri-user-fill text-gray-600 mr-1">{{ searchGuests.length }}</i> GUESTS </h1>
+          <div class="flex bg-white  rounded-lg gap-x-1 w-40">
+            <i class="ri-search-2-line pl-1 text-gray-400 mt-1"></i>
+            <input v-model="search" class=" w-full focus:outline-none" type="text" placeholder="search for guest" />
           </div>  
         </div>
-        <div class="flex justify-around">
-          <div class="bg-opacity-0 p-3 flex justify-around bg-blue-50 border-dotted border-2 rounded-xl m-2 w-40">
-            <p class="guest-text text-gray-700"> Total </p>
-            <i class="guest-text ri-user-fill text-gray-600 ">{{ searchGuests.length }}</i>
-          </div>
-          <div class="bg-opacity-0 p-3 flex justify-around bg-blue-50 m-2 w-40">
-            <button @click="toggleDark">
-                <i v-if="isDark" class="ri-sun-fill text-2xl text-orange-200"></i>
-                <i v-else class="ri-moon-clear-fill text-2xl text-gray-600"></i>
-            </button>
-          </div>
-        </div>
       </div>
+
   
       <ToastComponent v-if="showToast"/>
       <LoaderComponent v-if="isSigningOut"/>
       
-      <div class="max-w-2xl mx-auto mt-8 mb-16">
-        <div v-if="guests.length > 0" class="flex flex-col m-5">
-          <div class="overflow-x-auto shadow-md rounded-xl">
-              <div class="inline-block min-w-full align-middle">
-                  <div class="overflow-hidden">
-                      <table class="guests min-w-full divide-y divide-gray-200 table-fixed">
-                          <thead class="bg-blue-50">
-                              <tr>
-                                  <th scope="col" class="p-3">
-                                      <div class="flex items-center">
-                                          <!-- <input id="checkbox-all" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300"> -->
-                                          <!-- <label for="checkbox-all" class="sr-only">checkbox</label> -->
-                                      </div>
-                                  </th>
-                                  <th scope="col" class="py-3 px-3 text-xs font-bold tracking-wider text-left text-gray-700 uppercase">
-                                      Name
-                                  </th>
-                                  <th scope="col" class="py-3 px-3 text-xs font-bold tracking-wider text-left text-gray-700 uppercase">
-                                      Code
-                                  </th>
-                                  <th scope="col" class="py-3 px-3 text-xs font-bold tracking-wider text-left text-gray-700 uppercase">
-                                      Gender
-                                  </th>
-                                  <th scope="col" class="py-3 px-3 text-xs font-bold tracking-wider text-left text-gray-700 uppercase">
-                                      Time
-                                  </th>
-                              </tr>
-                          </thead>
-                         
-                          <tbody v-for="guest in searchGuests" :key="guest" class="divide-y divide-gray-200">
-                              <tr>
-                                <td class="p-3 w-3">
-                                  <div class="flex items-center">
-                                      <input v-model="checked" :value="guest" id="checkbox-table-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300">
-                                      <label for="checkbox-table-1" class="sr-only">checkbox</label>
-                                  </div>
-                                </td>
-                                <td class="guest-text py-4 px-3 text-xs font-medium whitespace-nowrap text-gray-500">{{ guest.name }}</td>
-                                <td class="guest-text py-4 px-3 text-xs font-medium whitespace-nowrap text-gray-500">{{ guest.code}}</td>
-                                <td class="guest-text py-4 px-3 text-xs font-medium whitespace-nowrap text-gray-500">{{ guest.gender }}</td>
-                                <td class="guest-text py-4 px-3 text-xs font-medium whitespace-nowrap text-gray-500">{{ guest.time }}</td>
-                              </tr>
-                          </tbody>
-                      </table>
-                  </div>
-              </div>
-          </div>
-        </div>
-  
-        <div v-else class="flex justify-center">
-          <div> 
-            <i class="ri-file-paper-line text-gray-300 text-7xl"></i>
-            <p class="font-serif text-gray-500">No Guests ! </p>
-          </div>
+      <div class="max-w-5xl mx-auto mt-8">
+      <div v-if="guests.length > 0" class="flex flex-col m-5">
+        <div class="overflow-x-auto shadow-md rounded-lg">
+            <div class="inline-block min-w-full align-middle">
+                <div class="overflow-hidden">
+                    <table class="guests min-w-full divide-y divide-gray-200 table-fixed">
+                        <thead class="bg-blue-50">
+                            <tr>
+                                <th scope="col" class="p-3">
+                                    <div class="flex items-center">
+                                        <!-- <input id="checkbox-all" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300"> -->
+                                        <!-- <label for="checkbox-all" class="sr-only">checkbox</label> -->
+                                    </div>
+                                </th>
+                                <th scope="col" class="py-3 px-1 text-xs font-bold tracking-wider text-left text-gray-700 uppercase">
+                                    Name
+                                </th>
+                                <th scope="col" class="py-3 px-1 text-xs font-bold tracking-wider text-left text-gray-700 uppercase">
+                                    Code
+                                </th>
+                                <th scope="col" class="py-3 px-1 text-xs font-bold tracking-wider text-left text-gray-700 uppercase">
+                                    Gender
+                                </th>
+                                <th scope="col" class="py-3 px-1 text-xs font-bold tracking-wider text-left text-gray-700 uppercase">
+                                    Time
+                                </th>
+                                <th scope="col" class="py-3 px-1 text-xs font-medium text-left text-gray-700 uppercase">
+                                    
+                                </th>
+                            </tr>
+                        </thead>
+                       
+                        <tbody v-for="guest in searchGuests" :key="guest" class="divide-y divide-gray-200">
+                            <tr>
+                              <div class="flex mt-5 justify-center">
+                                  <input v-model="checked" :value="guest" id="checkbox-table-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300">
+                                  <label for="checkbox-table-1" class="sr-only">checkbox</label>
+                              </div>
+                              <td class="guest-text py-4 px-2 text-xs font-medium whitespace-nowrap text-gray-500">{{ guest.name }}</td>
+                              <td class="guest-text py-4 px-2 text-xs font-medium whitespace-nowrap text-gray-500">{{ guest.code}}</td>
+                              <td class="guest-text py-4 px-2 text-xs font-medium whitespace-nowrap text-gray-500">{{ guest.gender }}</td>
+                              <td class="guest-text py-4 px-2 text-xs font-medium whitespace-nowrap text-gray-500">{{ guest.time }}</td>
+                              <td class="guest-text py-4 px-2 text-sm font-medium whitespace-nowrap"><i @click="deleteGuest(guest)" class="ri-delete-bin-5-fill text-red-400"></i></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
       </div>
-      <nav class="guests fixed bottom-0 right-0 left-0 bg-blue-50" aria-label="Page navigation">
-        <ul class="guests list-style-none flex justify-center gap-x-2 mt-2 mb-1 bg-blue-50">
-          <li>
-            <a
-              class="pointer-events-none relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-500 transition-all duration-300 dark:text-neutral-400"
-              >Previous</a
-            >
-          </li>
-          <li>
-            <a
-              class="relative block rounded bg-blue-100 px-3 py-1.5 text-sm font-medium text-blue-700 transition-all duration-300"
-              href="#!"
-              >1
-              <span
-                class="absolute -m-px h-px w-px overflow-hidden whitespace-nowrap border-0 p-0 [clip:rect(0,0,0,0)]"
-                >(current)</span
-              >
-            </a
-            >
-          </li>
-          <li aria-current="page">
-            <a
-            class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100  dark:text-gray-500 dark:hover:bg-blue-300 dark:hover:text-white"
-              href="#!"
-              >2 
-            </a>
-          </li>
-          <li>
-            <a
-              class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-gray-500 dark:hover:bg-blue-300 dark:hover:text-white"
-              href="#!"
-              >3</a
-            >
-          </li>
-          <li>
-            <a
-              class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-gray-500 dark:hover:bg-blue-300 dark:hover:text-white"
-              href="#!"
-              >Next</a
-            >
-          </li>
-        </ul>
-      </nav>
-    <!-- </div> -->
+
+      <div v-else class="flex justify-center">
+        <div> 
+          <i class="ri-file-paper-line text-gray-300 text-7xl"></i>
+          <p class="font-serif text-gray-500">No Guests ! </p>
+        </div>
+      </div>
+    </div>
   </template>
   
   <script setup>
@@ -141,17 +99,21 @@
   import { ref, onMounted, computed } from 'vue';
   import { db } from '../main.js';
   import { collection, getDocs } from 'firebase/firestore';
+  import { auth } from '@/main';
+  import { onAuthStateChanged } from '@firebase/auth';
   
-  const isDark = ref(false);
+  // const isDark = ref(false);
   const guests = ref([]);
+  const displayName = ref('');
   const checked = ref([])
   const search = ref('');
   const showToast = ref(false);
   const isSigningOut = ref(false);
+  const isLoggedIn = ref(false);
   
-  const toggleDark = () => {
-    isDark.value = !isDark.value;
-  }
+  // const toggleDark = () => {
+  //   isDark.value = !isDark.value;
+  // }
   
   const searchGuests = computed(() => {
     return guests.value.filter((guest) => {
@@ -160,7 +122,15 @@
   });
   
   onMounted(() => {
-    showGuest();
+    onAuthStateChanged(auth, (user) => {
+      if(user) {
+        isLoggedIn.value = true;
+        displayName.value = auth.currentUser.displayName;
+      } else {
+        isLoggedIn.value = false;
+      }
+      showGuest();
+    });
   });
   
   const showGuest = async() => {
